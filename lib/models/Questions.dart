b4291602366caf0b91e7fda0b7b5fec8e2c11456
gurnameh <gurunameh.work@gmail.com>
2021-03-07 19:50:29 +0530

    fixed git issues

--- lib/models/Questions.dart
+++ lib/models/Questions.dart
@@ -6,7 +6,49 @@ class Question {
   Question({this.id, this.question, this.answer, this.options});
 }
 
-const List sample_data = [
+var currDt = DateTime.now();
+// print(currDt.year); // 4
+// print(currDt.weekday); // 4
+// print(currDt.month); // 4
+// print(currDt.day); // 2
+// print(currDt.hour); // 15
+// print(currDt.minute); // 21
+// print(currDt.second); // 49
+List<String> days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+
+int day = currDt.weekday;
+String date = '$currDt.day';
+int month = currDt.month;
+int year = currDt.year;
+String time = '$currDt.hour';
+String place = '';
+String city = 'Chandigarh';
+String country = 'India';
+List sample_data = [
+  // {
+  //   "id": 1,
+  //   "question": "What is today's date?(from memory - no cheating!)",
+  //   "options": ['6th', '$day', '12th', '21th'],
+  //   "answer_index": 1,
+  // },
+  // {
+  //   "id": 2,
+  //   "question": "Which is the month?",
+  //   "options": ['$month', '${month + 1}', '${month + 2}', '${month + 3}'],
+  //   "answer_index": 0,
+  // },
+  // {
+  //   "id": 3,
+  //   "question": "What is the year?",
+  //   "options": ['${year - 1}', '$year', '${year + 1}', '${year + 2}'],
+  //   "answer_index": 1,
+  // },
+  // {
+  //   "id": 4,
+  //   "question": "What day is today?",
+  //   "options": ['${days[day - 1]}', '$days[day]', '${days[day + 1]}', '${days[day + 2]}'],
+  //   "answer_index": 1,
+  // },
   {
     "id": 1,
     "question": "Flutter is an open-source UI software development kit created by ______",
