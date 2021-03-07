23e149ac138faaed5b1ee7fc248f9adf13eb563b
gurnameh <gurunameh.work@gmail.com>
2021-03-07 18:29:25 +0530

    mid

--- lib/main.dart
+++ lib/main.dart
@@ -6,6 +6,7 @@ import 'package:alzhelp/services/auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:get/get_navigation/src/root/get_material_app.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -20,7 +21,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return StreamProvider<Users>.value(
       value: AuthService().user,
-      child: MaterialApp(
+      child: GetMaterialApp(
         debugShowCheckedModeBanner: false,
         home: Wrapper(),
         routes: {
